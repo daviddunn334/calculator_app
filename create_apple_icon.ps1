a8a4da2cc471703e8217a41062971e5e4aa3dd80
David Dunn <daviddunn334@gmail.com>
2026-02-09 01:32:12 -0600

    Increase PWA app icon logo size - Logo now fills 85% of maskable icons (up from ~60%) for better visibility when added to homescreen

---
+++ create_apple_icon.ps1
@@ -0,0 +1,24 @@
+Add-Type -AssemblyName System.Drawing
+
+$source = [System.Drawing.Image]::FromFile((Resolve-Path "assets\logos\logo_main.png"))
+$canvas = New-Object System.Drawing.Bitmap(180, 180)
+$graphics = [System.Drawing.Graphics]::FromImage($canvas)
+$graphics.InterpolationMode = [System.Drawing.Drawing2D.InterpolationMode]::HighQualityBicubic
+$graphics.SmoothingMode = [System.Drawing.Drawing2D.SmoothingMode]::HighQuality
+
+# Background color
+$brush = New-Object System.Drawing.SolidBrush([System.Drawing.Color]::FromArgb(27, 50, 91))
+$graphics.FillRectangle($brush, 0, 0, 180, 180)
+
+# Logo at 85%
+$logoSize = [int](180 * 0.85)
+$offset = [int]((180 - $logoSize) / 2)
+$graphics.DrawImage($source, $offset, $offset, $logoSize, $logoSize)
+
+$canvas.Save("web\icons\apple-touch-icon.png", [System.Drawing.Imaging.ImageFormat]::Png)
+$graphics.Dispose()
+$canvas.Dispose()
+$brush.Dispose()
+$source.Dispose()
+
+Write-Output "Created apple-touch-icon.png (180x180 with 85% logo for iOS)"
